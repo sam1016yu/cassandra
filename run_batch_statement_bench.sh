7341a38a923a53442cd914df06380aefc27c4b0d
sam1016yu <miaoyu1016@gmail.com>
2023-05-02 17:51:23 +0000

    change mode

--- run_batch_statement_bench.sh
+++ run_batch_statement_bench.sh
@@ -4,7 +4,7 @@ cd $HOME/cassandra
 ant build-jmh
 CP="$HOME/cassandra/build/classes/main:$HOME/cassandra/build/classes/thrift:$HOME/cassandra/lib/*:$HOME/cassandra/build/lib/jars/*:$HOME/cassandra/build/test/classes:$HOME/cassandra/test/conf"
 java -classpath $CP org.openjdk.jmh.Main .*microbench.*BatchStatementBench -prof gc 2>&1 | tee bench_output.log
-grep "^BatchStatementBench.bench" bench_output.log > $HOME/result_before.out
+grep "^BatchStatementBench.bench" bench_output.log > $HOME/result_after.out
 rm bench_output.log
 ant clean
 cd $cur_dir
