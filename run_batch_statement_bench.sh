870839016662fe57dc187ee99eefa2e42275edb0
Miao Yu <miaoyu1016@gmail.com>
2023-05-02 15:35:57 -0400

    Update run_batch_statement_bench.sh

--- run_batch_statement_bench.sh
+++ run_batch_statement_bench.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cur_dir=`pwd`
 cd $HOME/cassandra
-ant build-jmh
+echo "Building..."
+ant build-jmh > /dev/null
+echo "Running BatchStatementBench..."
 CP="$HOME/cassandra/build/classes/main:$HOME/cassandra/build/classes/thrift:$HOME/cassandra/lib/*:$HOME/cassandra/build/lib/jars/*:$HOME/cassandra/build/test/classes:$HOME/cassandra/test/conf"
 java -classpath $CP org.openjdk.jmh.Main .*microbench.*BatchStatementBench -prof gc  &>  bench_output.log
 grep "^BatchStatementBench.bench" bench_output.log > $HOME/result_after.out
+echo "Done, cleaning now"
 rm bench_output.log
-ant clean
+ant clean > /dev/null
 cd $cur_dir
