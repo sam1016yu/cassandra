0b6e0aaec116a35ce67129c938304421749dd70a
sam1016yu <miaoyu1016@gmail.com>
2023-05-02 13:28:57 +0000

    record output

--- run_batch_statement_bench.sh
+++ run_batch_statement_bench.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 CP="$HOME/cassandra/build/classes/main:$HOME/cassandra/build/classes/thrift:$HOME/cassandra/lib/*:$HOME/cassandra/build/lib/jars/*:$HOME/cassandra/build/test/classes:$HOME/cassandra/test/conf"
-java -classpath $CP org.openjdk.jmh.Main .*microbench.*BatchStatementBench -prof gc
+java -classpath $CP org.openjdk.jmh.Main .*microbench.*BatchStatementBench -prof gc 2>&1 | tee bench_output.log
