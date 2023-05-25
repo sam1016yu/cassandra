03ef1db25815dbd03bdd3a1dc4e470cc00708119
sam1016yu <miaoyu1016@gmail.com>
2023-05-25 01:27:20 +0000

    add run scripot

---
+++ run_patch_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd $HOME/cassandra
+ant clean > /dev/null
+
+result_file=raw_result_after
+
+if [ -d $result_file ]; then
+    rm -f $result_file
+fi
+
+ant testsome \
+-Dtest.name=org.apache.cassandra.io.sstable.SSTableMetadataTest \
+-Dtest.methods=trackClusteringValuesMinimize \
+&> raw_output.log
+
+grep -F '!!!' raw_output.log > $result_file
+
+rm raw_output.log
