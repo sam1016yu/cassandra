448947a1bb2354ead5e0a504893e95fd3277ec85
sam1016yu <miaoyu1016@gmail.com>
2023-06-07 14:22:31 +0000

    update runner script

--- run_patch_test.sh
+++ run_patch_test.sh
@@ -5,8 +5,20 @@ export JAVA8_HOME='/usr/lib/jvm/java-8-openjdk-amd64'
 export JAVA11_HOME='/usr/lib/jvm/java-11-openjdk-amd64' 
 export JDK_HOME='/usr/lib/jvm/java-8-openjdk-amd64'
 
+
+cd $HOME/cassandra
+ant clean > /dev/null
+result_file=raw_result_before
+
+if [ -d $result_file ]; then
+    rm -f $result_file
+fi
+
 ant testsome \
 -Dtest.name=org.apache.cassandra.repair.RepairJobCustomTest \
 -Dtest.methods=releaseThreadAfterSessionForceShutdown \
-2>&1 | tee raw_output.log
-# &> raw_output.log
+&> raw_output.log
+
+grep -F '!!!' raw_output.log > $result_file
+rm raw_output.log
+mv $result_file $HOME
